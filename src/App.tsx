af9865359274e539fda307f4b8c46b4d95967f0e
fangfelipe <fangfelipe@gmail.com>
2021-01-14 22:04:52 +0800

    update__react-router

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,6 @@ interface AppProps {
 
 const App: React.FC<AppProps> = ({ }) => {
 
-    let routerConfig = [
-        { path: "/", title: "首頁", exact: true, component: Index },
-        { path: "/video", title: "教學影片", exact: false, component: Video },
-        { path: "/workplace", title: "技能樹", exact: false, component: Workplace }
-    ]
 
     return (
         <BrowserRouter>
@@ -23,23 +18,43 @@ const App: React.FC<AppProps> = ({ }) => {
                 <div className="leftNav">
                     <h3>導覽</h3>
                     <ul>
-                        {routerConfig.map((item, index) => (
-                            <li key={index}>
-                                <Link to={item.path}>
-                                    {item.title}
-                                </Link>
-                            </li>
-                        ))}
+
+                        <li>
+                            <Link to={{
+                                pathname: "/",
+                                state: {
+                                    form: "home"
+                                }
+                            }}>
+                                首頁
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/video">
+                                教學影片
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/workplace">
+                                技能樹
+                            </Link>
+                        </li>
+
                     </ul>
                 </div>
                 <div className="rightMain">
-                    {routerConfig.map((item, index) => (
-                        <Route
-                            key={index}
-                            component={item.component}
-                            exact={item.exact}
-                            path={item.path} />
-                    ))}
+
+                    <Route
+                        component={Index}
+                        exact
+                        path="/" />
+                    <Route
+                        component={Video}
+                        path="/video" />
+
+                    <Route
+                        component={Workplace}
+                        path="/workplace" />
                 </div>
             </div>
         </BrowserRouter>
--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 const Index = () => {
+    let location = useLocation()
+    console.log({ location });
     return (
         <h2>我是首頁</h2>
     )
